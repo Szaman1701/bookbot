aa03eb1248d818113935751f2dce4c4046dbbddc
Szaman1701 <daimonfrey1701@gmail.com>
2025-04-04 16:33:01 +0200

    B: Added number of words function

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def get_book_text(path_to_file):
 def main():
     print(get_book_text("./books/frankenstein.txt"))
 
-main()
+def number_of_words():
+    num_words = len(get_book_text("./books/frankenstein.txt").split())
+    print(num_words,"words found in the document")
+
+#main()
+number_of_words()
 
