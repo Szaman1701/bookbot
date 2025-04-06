15cb01738d01dff4b51ce5001557479fae6798e3
Szaman1701 <daimonfrey1701@gmail.com>
2025-04-06 14:18:24 +0200

    B: added second file and moved functions

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import number_of_words
+
+# def get_book_text(path_to_file):
+#     with open(path_to_file) as f:
+#         file_contents = f.read()
+#     return file_contents
 
 def main():
     print(get_book_text("./books/frankenstein.txt"))
 
-def number_of_words():
-    num_words = len(get_book_text("./books/frankenstein.txt").split())
-    print(num_words,"words found in the document")
+# def number_of_words():
+#     num_words = len(get_book_text("./books/frankenstein.txt").split() )
+#     print(num_words,"words found in the document")
 
 #main()
 number_of_words()
---
+++ stats.py
@@ -0,0 +1,10 @@
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def number_of_words():
+    num_words = len(get_book_text("./books/frankenstein.txt").split() )
+    print(num_words,"words found in the document")
